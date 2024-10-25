f4eefaa6596ac656e994e8a18d4503791bbd064e
Bimal dev <bimaldev707@gmail.com>
2024-10-25 07:06:57 +0530

    server created

---
+++ server/server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser =require("cookie-parser");
+const cors = require("cors")
+
+
+
+const app = express()
+const PORT = process.env.PORT || 5000
+mongoose.connect("mongodb+srv://bimaldev707:Bimal%402001@cluster0.qedlv.mongodb.net/")
+.then(()=> console.log("Mongo Db connected"))
+.catch(()=>console.log(error))
+
+
+
+app.use(cors ({
+    origin:'http://localhost:5173/',
+    methods:['GET', 'POST', 'DELETE', 'PUT'],
+    allowedHeaders:[
+        "Content-Type",
+        "Authorization",
+        "Cache-Control",
+        "Expires",
+        "Pragma"
+    ],
+    credentials:true
+}))
+
+
+app.use(cookieParser());
+app.use(express());
+app.listen(PORT, () => console.log(`server is running in port ${PORT}`)
+)
