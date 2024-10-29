9d555a490aa1db9b92fbeae767a9d108b80a11d8
Bimal dev <bimaldev707@gmail.com>
2024-10-29 18:07:53 +0530

    Layout designed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {Routes, Route } from 'react-router-dom'
+import Layout from './Auth/Layout'
+import Login from './Pages/Auth/Login'
+import Register from './Pages/Auth/Register'
+import React from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      </>
+    <div className='flex flex-col overflow-hidden bg-white'>
+      <h1>Header Component</h1>
+      <Routes>
+        {/* Define the Layout as a parent route for Auth */}
+        <Route path="/auth" element={<Layout />}>
+          {/* Nested routes for Login and Register */}
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Route>
+      </Routes>
+    </div>
   )
 }
 
