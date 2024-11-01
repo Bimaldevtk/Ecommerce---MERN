e3fd086bd439df619a786da5a96a6ab79ceb801f
Bimal dev <bimaldev707@gmail.com>
2024-11-01 16:04:26 +0530

    Check Auth implemented for security

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { Routes, Route, Router } from "react-router-dom";
-import Layout from "./Auth/Layout";
+import AuthLayout from "./Auth/Layout";
 import Login from "./Pages/Auth/login";
 import Register from "./Pages/Auth/Register";
 import React from "react";
@@ -11,27 +11,65 @@ import AdminOrders from "./components/Admin_view/Orders";
 import ShoppingLayout from "./components/Shopping_view/Layout";
 import ShoppingHeader from "./components/Shopping_view/Header";
 import Not_found from "./Pages/Not_found/Not_found";
+import ShoppingHome from "./Pages/Shopping_view/Home";
+import ShoppingCheckout from "./Pages/Shopping_view/Checkout";
+import ShoppingListing from "./Pages/Shopping_view/Listing";
+import ShoppingAccount from "./Pages/Shopping_view/Account";
+import CheckAuth from "./components/Common/Check-Auth";
+import UnAuthPage from "./Pages/Unauth_page";
 
 function App() {
+  const isAuthenicated = true;
+  const user = {
+    name:'Bimal',
+    role:'user'
+  };
+
   return (
     <div className="flex flex-col overflow-hidden bg-white">
       <h1>Header Component</h1>
       <Routes>
         {/* Define the Layout as a parent route for Auth */}
-        <Route path="/auth" element={<Layout />}>
+        <Route
+          path="/auth"
+          element={
+            <CheckAuth  isAuthenicated={isAuthenicated} user={user}>
+              <AuthLayout />
+            </CheckAuth>
+          }
+        >
           {/* Nested routes for Login and Register */}
           <Route path="login" element={<Login />} />
           <Route path="register" element={<Register />} />
         </Route>
-        <Route path="/admin" element={<AdminLayout />}>
+        <Route
+          path="/admin"
+          element={
+            <CheckAuth isAuthenicated={isAuthenicated} user={user}>
+              <AdminLayout />
+            </CheckAuth>
+          }
+        >
           <Route path="dashboard" element={<AdminDashboard />} />
           <Route path="features" element={<AdminFeatures />} />
           <Route path="orders" element={<AdminOrders />} />
         </Route>
-        <Route path="/shop" element={<ShoppingLayout />}>
+        <Route
+          path="/shop"
+          element={
+            <CheckAuth isAuthenicated={isAuthenicated} user={user}>
+              <ShoppingLayout />
+            </CheckAuth>
+          }
+        >
           <Route path="header" element={<ShoppingHeader />} />
+          <Route path="checkout" element={<ShoppingCheckout />} />
+          <Route path="lisiting" element={<ShoppingListing />} />
+          <Route path="account" element={<ShoppingAccount />} />
+          <Route path="home" element={<ShoppingHome />} />
         </Route>
-        <Route path="*" element ={<Not_found/>}/> 
+        <Route path="*" element={<Not_found />} />
+        <Route path="/unauth" element ={<UnAuthPage/>} />
       </Routes>
     </div>
   );
---
+++ client/src/Pages/Unauth_page/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UnAuthPage = () => {
+  return (
+    <div>
+      You have no right to use this page !!!!       
+    </div>
+  )
+}
+
+export default UnAuthPage
